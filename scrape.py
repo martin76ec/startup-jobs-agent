0098b5ea9f7b55d96148737ec4a270c6ea46e775
Ralph Coaquira <ralph.coaquira.rubin@gmail.com>
2025-02-11 15:02:57 -0500

    se agrega paquete os, dotenv para incluir variables de entorno, se logra abrir web de Linkedin

--- scrape.py
+++ scrape.py
@@ -1,7 +1,16 @@
+import os
+from dotenv import load_dotenv # type: ignore
 import selenium.webdriver as webdriver # type: ignore
 from selenium.webdriver.chrome.service import Service # type: ignore
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
+
+# url -> https://www.linkedin.com/jobs/search/?currentJobId=4131896161&distance=25&geoId=102927786&keywords=product%20manager&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true
+
+load_dotenv()
+LINKEDIN_EMAIL = os.getenv("LINKEDIN_EMAIL")
+LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD")
 
 def scrape_website(website):
     print("lauching chrome browser...")
@@ -25,8 +34,22 @@ def scrape_website(website):
         driver.get(website)
         print("Page loaded...")
         html = driver.page_source
-        time.sleep(10)
 
+        # nuevo codigo para logueo
+        button = driver.find_element(By.XPATH, '//*[@id="base-contextual-sign-in-modal"]/div/section/div/div/div/div[2]/button')
+        button.click()
+        time.sleep(5)
+        user = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_key"]')
+        user.send_keys(LINKEDIN_EMAIL)
+
+        pwd = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_password"]')
+        pwd.send_keys(LINKEDIN_PASSWORD)
+        time.sleep(2)
+
+        sbm = driver.find_element(By.XPATH,'//*[@id="base-sign-in-modal"]/div/section/div/div/form/div[2]/button')
+        sbm.click()
+
+        time.sleep(30)
         return html
-    finally:
-        driver.quit()
+    # finally:
+    #     driver.quit()
