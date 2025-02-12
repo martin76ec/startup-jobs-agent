d3952df0a4f83957d188ebdc1ccef2a412ce6b98
martin76ec <larreaamartin@gmail.com>
2025-02-12 18:22:46 -0500

    feat: add job parser

--- src/domain/jobs.py
+++ src/domain/jobs.py
@@ -1,18 +1,68 @@
-from src.constants.env import DB_ID
+from typing import Any, Dict, Optional, cast
+from src.constants.env import DB_ID, NOTION_KEY
 from src.infrastructure.notion import notion
+from dataclasses import dataclass
+
+
+@dataclass
+class NotionData:
+    apply_url: Optional[str]
+    company_hq: Optional[str]
+    date_scrapped: Optional[str]
+    details: Optional[str]
+    id: Optional[int]
+    name: Optional[str]
+    remote: Optional[str]
+    startup: Optional[str]
+    status: Optional[str]
+    vertical: Optional[str]
+
+
+def job_raw_to_obj(raw_job: Dict[str, Any]) -> NotionData:
+    return NotionData(
+        apply_url=raw_job.get("properties", {}).get("Apply URL", {}).get("url"),
+        company_hq=raw_job.get("properties", {})
+        .get("Company HQ", {})
+        .get("rich_text", [{}])[0]
+        .get("plain_text"),
+        date_scrapped=raw_job.get("properties", {})
+        .get("Date Scrapped", {})
+        .get("created_time"),
+        details=raw_job.get("properties", {})
+        .get("Details", {})
+        .get("rich_text", [{}])[0]
+        .get("plain_text"),
+        id=raw_job.get("properties", {})
+        .get("ID", {})
+        .get("unique_id", {})
+        .get("number"),
+        name=raw_job.get("properties", {})
+        .get("Name", {})
+        .get("title", [{}])[0]
+        .get("plain_text"),
+        remote=raw_job.get("properties", {})
+        .get("Remote", {})
+        .get("select", {})
+        .get("name"),
+        startup=raw_job.get("properties", {})
+        .get("Startup", {})
+        .get("rich_text", [{}])[0]
+        .get("plain_text"),
+        status=raw_job.get("properties", {})
+        .get("Status", {})
+        .get("status", {})
+        .get("name"),
+        vertical=raw_job.get("properties", {})
+        .get("Vertical", {})
+        .get("select", {})
+        .get("name"),
+    )
 
 
 async def jobs_get():
-    job_list = notion.databases.query(
-        **{
-            "database_id": DB_ID,
-            "filter": {
-                "property": "Status",
-                "rich_text": {
-                    "equals": "In Review",
-                },
-            },
-        }
-    )
+    query = cast(dict, notion.databases.query(database_id=DB_ID))
+    results = query["results"]
+    jobs = [job_raw_to_obj(r) for r in results]
+    breakpoint()
+    return jobs
 
-    return job_list
