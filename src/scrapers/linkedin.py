731ac46ad90f273915501372c27fabfd9f82438c
Ralph Coaquira <ralph.coaquira.rubin@gmail.com>
2025-02-20 23:21:54 -0500

    scraper para dar click al boton 'ver mas', test 1

--- src/scrapers/linkedin.py
+++ src/scrapers/linkedin.py
@@ -1,6 +1,8 @@
 import selenium.webdriver as webdriver  # type: ignore
 from selenium.webdriver.chrome.service import Service  # type: ignore
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from src.constants.env import CHROME_BINARY_PATH, CHROME_DRIVER_PATH, LINKEDIN_EMAIL, LINKEDIN_PASSWORD
 import time
@@ -18,14 +20,15 @@ def scrape_website(website):
     options = Options()
     options.binary_location = chrome_binary_path
     options.add_argument("--no-sandbox")
+    # options.add_argument("--headless")
 
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
     try:
         try:
             driver.get(website)
+            wait = WebDriverWait(driver, 10)
             print("Loading page...")
-            html = driver.page_source
 
             # nuevo codigo para logueo
             button = driver.find_element(
@@ -102,18 +105,56 @@ def scrape_website(website):
         except:
             job_data["seniority"] = "null"
 
-        try:
+        """ try:
             # encontrar la descripcion
             print("Reading description...")
-            description = driver.find_element(
-                By.XPATH, '//*[@id="job-details"]/div/p'
-            ).text
+            # description = driver.find_element(By.ID, "job-details").text
+            job_description_element = driver.find_element(By.TAG_NAME, 'html').text
+            print(job_description_element)
+            print("----------")
+
+            paragraphs = job_description_element.find_elements(By.TAG_NAME, "p")
+            description = "\n".join([p.text for p in paragraphs if p.text.strip()])
+
             job_data["description"] = description
+            print(description)
+            print("----------")
             desc = job_summarize_description(description)
             job_data["description"] = desc
 
-        except:
-            job_data["description"] = "null"
+        except Exception as e:
+            print(e)
+            job_data["description"] = "null" """
+
+        try:
+            # encontrar la descripcion
+            print("Reading description...")
+            breakpoint()
+            # Esperar hasta que la descripción del trabajo esté presente
+            job_description_element = wait.until(
+                EC.presence_of_element_located((By.ID, "job-details"))
+            )
+            # Buscar el botón padre que contiene al span con clase "see-more"
+            try:
+                see_more_button = wait.until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, "text-expand-collapse-button"))
+                )
+                if see_more_button.is_displayed():
+                    see_more_button.click()
+                    time.sleep(2)  # Esperar a que se expanda el contenido
+            except Exception as e:
+                print("No se encontró el botón 'Ver más' o no se pudo hacer clic.")
+
+            # Buscar el elemento que contiene la descripción del trabajo
+            job_description_element = driver.find_element(By.ID, "job-details")
+            paragraphs = job_description_element.find_elements(By.TAG_NAME, "p")
+
+            job_description = "\n".join([p.text for p in paragraphs if p.text.strip()])
+
+            return job_description
+        except Exception as e:
+            print(f"Error al obtener la descripción: {e}")
+            return None
 
         time.sleep(10)
         return job_data
@@ -128,4 +169,5 @@ def scrape_website(website):
         return None
 
     finally:
-        driver.quit()  # Cierra el navegador
+        print("casi terminamosss")
+        # driver.quit()  # Cierra el navegador
